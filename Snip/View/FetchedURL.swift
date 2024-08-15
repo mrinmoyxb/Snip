fa4187c9a8d5c586bfc530fed2f9b70b8d97c85a
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-08-15 23:43:28 +0530

    updated the API Model

---
+++ Snip/View/FetchedURL.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Snip
+//
+//  Created by Mrinmoy Borah on 15/08/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
