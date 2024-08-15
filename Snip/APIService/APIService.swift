959aec0997de393e245921c6b737d41a00c88497
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-08-15 14:51:32 +0530

    completed the API service for GET request

--- Snip/APIService/APIService.swift
+++ Snip/APIService/APIService.swift
@@ -6,6 +6,24 @@
 //
 
 import Foundation
-class APIViewModel: ObservableObject{
-    
+class APIService {
+    func getAllUrls() async throws -> [URLModel]{
+        let urlString = "http://localhost:8001/api/url/"
+        guard let url = URL(string: urlString) else{
+            throw URLError(.badURL)
+        }
+        
+        let(data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse =  response as? HTTPURLResponse, httpResponse.statusCode == 200 else{
+            dump(response)
+            return []
+        }
+        
+        let urlData = try JSONDecoder().decode(AllURLModel.self, from: data)
+        let fetchedUrls: [URLModel] = urlData.msg.flatMap{ msg -> [URLModel] in
+            return [msg]
+        }
+        return fetchedUrls
+    }
 }
+
