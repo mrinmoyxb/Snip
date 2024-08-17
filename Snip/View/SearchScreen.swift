b5c9a6d51c25548ecc45473c41388c67b6695ffe
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-08-17 11:04:05 +0530

    created tab view and fixed fetch url ui

---
+++ Snip/View/SearchScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SearchScreen.swift
+//  Snip
+//
+//  Created by Mrinmoy Borah on 17/08/24.
+//
+
+import SwiftUI
+
+struct SearchScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchScreen()
+}
